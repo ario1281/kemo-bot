44fd2d074a9e6b2a798cb705a102c6d5fb90eaea
DESKTOP-A7APPNE\ario1281 <yakumo.ario1281@gmail.com>
2026-01-03 01:40:36 +0900

    u2

--- app/index.js
+++ app/index.js
@@ -18,21 +18,19 @@ client.once("ready", () => {
 
 client.on("messageCreate", async (msg) => {
     // Botまたは自分のメッセージは無視
-    if(msg.author.id == client.user.id || msg.author.bot) { return; }
+    if (msg.author.id == client.user.id || msg.author.bot) { return; }
 
     // コマンド処理
-    if(msg.content.startsWith(config.prefix)) {
+    if (msg.content.startsWith(config.prefix)) {
         const content = msg.content.substring(1, msg.content.length);
         const args = content.split(" ");
         const cmd = args.shift().toLowerCase();
 
-        if (cmd === "hello")
-        {
+        if (cmd === "hello") {
             const line = hello_lines[Math.floor(Math.random() * hello_lines.length)];
             await msg.channel.send(`${line} 🦊`);
         }
-        if (cmd === "dice")
-        {
+        if (cmd === "dice") {
             const face = parseInt(args[0]) || 6;
             const value = Math.floor(Math.random() * face) + 1;
             await msg.channel.send(`🎲 ${face}面サイコロで、"${value}"の目が出たのじゃ！`);
