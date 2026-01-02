dd606af2597dbfcbf95723df0b67d4d157edf5bc
DESKTOP-A7APPNE\ario1281 <yakumo.ario1281@gmail.com>
2026-01-02 21:31:02 +0900

    Init repository

--- information.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-function sendMsg(chID,text, option={}) {
-    client.channels.get(chID).send(text, option)
-    .then(console.log("メッセージ送信" + text + JSON.stringify(option)))
-    .catch(console.error);
-}
