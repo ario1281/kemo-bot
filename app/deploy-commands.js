538db018ff644bf7e5fdbc8fb9a9df6332b34f7e
DESKTOP-A7APPNE\ario1281 <yakumo.ario1281@gmail.com>
2026-01-03 01:07:04 +0900

    Guildコマンドの追加

--- app/deploy-commands.js
+++ app/deploy-commands.js
@@ -18,10 +18,23 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 // グローバルコマンドとして登録
 await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID),
+    Routes.applicationCommands(
+      process.env.CLIENT_ID
+    ),
     { body: cmds },
 );
 
 console.log("✅ Globalコマンド登録完了（反映まで数分〜最大1時間）");
+
+// ギルドコマンドとして登録
+await rest.put(
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID, 
+      process.env.GUILD_ID
+    ),
+    { body: cmds },
+);
+
+console.log("✅ Guildコマンド登録完了");
 
 // end of app/deploy-commands.js
