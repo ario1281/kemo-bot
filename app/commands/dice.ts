4626008118114d6fab08ccd48764564b888b3708
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2026-01-14 11:25:16 +0900

    Update dice.ts

--- app/commands/dice.ts
+++ app/commands/dice.ts
@@ -2,11 +2,11 @@ import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
-        .setName("dice")
-        .setDescription("サイコロを振るのじゃ！")
+        .setName("roll")
+        .setDescription("骰子(さいころ)を振るのじゃ！")
         .addIntegerOption(option =>
-            option.setName("face")
-                .setDescription("サイコロの面の数　デフォルト:6")
+            option.setName("dice")
+                .setDescription("ダイスロール　デフォルト:1d6")
                 .setRequired(false)
         ),
 
@@ -14,10 +14,29 @@ export default {
         // オプション確認用ログ
         console.log("name:", this.data.name);
 
-        // 
-        const face = inter.options.getInteger("face") || 6;
-        const value = Math.floor(Math.random() * face) + 1;
+        // ダイス
+        const roll = inter.options.getString("dice") || "1d6";
 
-        await inter.reply(`🎲 ${face}面サイコロで、"${value}"の目が出たのじゃ！`);
+        const regex = /^(?<item>\d+)d(?<face>\d+)$/i;
+        const match = regex.exec(roll);
+
+        if (!match || !match.groups) {
+            await inter.reply(`❓ よく分からなかったのじゃ…`);
+            return;
+        }
+
+        const item = parseInt(match.groups.item);
+        const face = parseInt(match.groups.face);
+        const rolls[];
+        for (let i = 0; i < item; i++) {
+            const val = Math.floor(Math.random() * face) + 1;
+            sum += val;
+            rolls.push(val);
+        }
+
+        await inter.reply([
+            `🎲 ${face}面サイコロ${item}個を振ったのじゃ！！`,
+            `[${rolls.join(", ")}] = ${rolls.reduce((a, c) => a + c, 0)}`
+        ].join("\n"));
     }
 };
